ec485a4564abb31b00eb45426f2c21362a0a1336
BigTallahasee <85016240+BigTallahasee@users.noreply.github.com>
2022-09-13 10:11:20 -0500

    Update ZCorpsEveEsi.java

--- src/main/java/zcorps/eve_esi/ZCorpsEveEsi.java
+++ src/main/java/zcorps/eve_esi/ZCorpsEveEsi.java
@@ -4,10 +4,10 @@ package zcorps.eve_esi;
  * @author Zachary Nichelson (Lurderker Etgur Avuli)
  */
 public interface ZCorpsEveEsi {
-    public final String CLIENT_ID = "ef8b4e82383e4173bb99983165a37c03";
+    public String CLIENT_ID = "ef8b4e82383e4173bb99983165a37c03";
     public final String CLIENT_ID_URL = "&client_id=";
     public final String RESPONSE_TYPE = "?response_type=code";
-    public final String CALLBACK_URL = "eveauth-app://callback/";
+    public String CALLBACK_URL = "eveauth-app://callback/";
     public final String REDIRECT = "&redirect_uri="+ CALLBACK_URL;
     public final String SCOPE = "&scope=";
     public final String CODE_CHALLENGE = "&code_challenge=";
